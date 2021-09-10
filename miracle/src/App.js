6557c3bb447f233c58380c058d13875fd8a0b3ff
alex.zaharov <alex.zaharov@accenture.com>
2021-09-10 19:26:26 +0300

    Install commit

---
+++ miracle/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content-wrapper">
+          <div className="Message">
+              <h1>Miracle.by</h1>
+              <h3>Сайт находится в стадии разработки</h3>
+              <p>Скоро все заработает - обязательно возвращайтесь!</p>
+          </div>
+          <div className="contact">
+              <p>Вы можете связваться со мной:</p>
+              <a href="mailto:zakharov.faur@gmail.com">email: Zakharov.faur@gmail.com</a>
+              <a href="https://t.me/zakharov_faur">Telegram: @zakharov_faur</a>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
