59bb02eae36b657f2334b99573fce37dcf0cabb6
alex.zaharov <alex.zaharov@accenture.com>
2021-09-28 22:48:17 +0300

    change style for why block

--- miracle/src/components/Why.jsx
+++ miracle/src/components/Why.jsx
@@ -3,32 +3,31 @@ export default function Why() {
         <div className="why-block">
             <div className="container-wrapper why-wrapper">
                 <div className="title-block why-title-block">
-                    <h2 className="title-simple title-simple-why">Почему следует выбрать нас?</h2>
+                    <h2 className="title-simple title-simple-why">Почему следует <span className="select">выбрать</span> нас?</h2>
                     <h3 className="title-secondary-simple title-simple-why">
                         Мы гарантируем качественное исполнение
                     </h3>
-                    <img className="why-img" src="assets/work/tr.png" alt="smoke"/>
                 </div>
                 <div className="why-content">
-                    <div className="why-card">
+                    <div className="why-card card-one">
                         <h3 className="why-title">Лучшие цены</h3>
-                        <p className="why-text">Мы молодая команда профисионалов,
-                            которая наработывает клиентскую базу,
+                        <p className="why-text">Мы молодая команда профессионалов,
+                            которая нарабатывает клиентскую базу,
                             поэтому предлагаем лучшие цены.</p>
                     </div>
 
-                    <div className="why-card">
+                    <div className="why-card card-two">
                         <h3 className="why-title">Оплата после передачи сайта</h3>
                         <p className="why-text">В отличии от своих конкурентов мы не берем предоплату.</p>
                     </div>
 
-                    <div className="why-card">
+                    <div className="why-card card-three">
                         <h3 className="why-title">Современные технологии</h3>
                         <p className="why-text">Используем самые современные стандарты
                             по разработке сайтов и web приложений.</p>
                     </div>
 
-                    <div className="why-card">
+                    <div className="why-card card-four">
                         <h3 className="why-title">Общение с клиентом</h3>
                         <p className="why-text">Информируем клиента обо всех этапах разработки.</p>
                     </div>
